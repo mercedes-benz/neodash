2574e1c6f0f79d1549d48fd05119ff4cf720d2a1
Monish <139316519+m-o-n-i-s-h@users.noreply.github.com>
2023-07-31 18:35:07 +0530

    Fixed failing testcase

--- cypress/e2e/start_page.cy.js
+++ cypress/e2e/start_page.cy.js
@@ -85,7 +85,7 @@ describe('NeoDash E2E Tests', () => {
       .and('contain', 'released')
       .and('not.contain', '__id');
     cy.get('main .react-grid-item:eq(2) .MuiDataGrid-virtualScroller .MuiDataGrid-row').should('have.length', 3);
-    cy.get('main .react-grid-item:eq(2) .MuiDataGrid-footerContainer').should('contain', '1–5 of 8');
+    cy.get('main .react-grid-item:eq(2) .MuiDataGrid-footerContainer').should('contain', '1–3 of 8');
     cy.get('main .react-grid-item:eq(2) .MuiDataGrid-footerContainer button[aria-label="Go to next page"]').click();
     cy.get('main .react-grid-item:eq(2) .MuiDataGrid-virtualScroller .MuiDataGrid-row').should('have.length', 3);
     cy.get('main .react-grid-item:eq(2) .MuiDataGrid-footerContainer').should('contain', '6–8 of 8');
