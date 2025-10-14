ba783e487e36e17175c8d8f8c9043b012e0e4cc4
Suhas <harman.matapathi@mercedes-benz.com>
2025-10-14 12:20:11 +0530

    fix: per comments

--- src/extensions/styling/StyleRuleEvaluator.ts
+++ src/extensions/styling/StyleRuleEvaluator.ts
@@ -58,7 +58,7 @@ export const evaluateRulesOnMappedNeo4jRecord = (record, mapping, customization,
  * @returns the index of the rule that is satisfied.
  */
 export const evaluateRulesOnDict = (dict, rules, customizations) => {
-  if (!dict || !rules || !customizations) {
+  if (!dict || !rules) {
     return -1;
   }
   for (const [index, rule] of rules.entries()) {
